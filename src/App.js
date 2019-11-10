1f40912cad65923551caec29158858c1461c5bf4
Daria Kiritseva <dakirits@gmail.com>
2019-11-10 15:13:10 -0800

    added navbar, routing, link to movie title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import './App.css';
-import Movies from './components/movies';
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import "./App.css";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Rentals from "./components/rentals";
+import Customers from "./components/customers";
+import NavBar from "./components/common/navbar";
+import NotFound from "./components/common/notFound";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" component={Movies} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
